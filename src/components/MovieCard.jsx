b5152fd9d89a1115b11cbed2b04dc8964dccfc2c
Sharath M <youremail@example.com>
2025-07-03 17:04:34 +0530

    Complete project upload

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
+// src/components/MovieCard.jsx
+import { Link } from 'react-router-dom';
+import React from 'react';
 
-const MovieCard = ({movie : {title, vote_average, poster_path, release_date, original_language}}) => {
+const MovieCard = ({ movie }) => {
+    const { id, poster_path, title, vote_average, release_date, original_language } = movie;
+
     return (
-<div className='movie-card'>
-    <img
-        src={poster_path ?
-            `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
-        alt={title}
-    />
-    <div className="mt-4">
-        <h3>{title}</h3>
+        <li className="movie-card">
+            <Link to={`/movie/${id}`}>
+                <div>
+                    <img
+                        src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+                        alt={title}
+                    />
+                    <div className="mt-4">
+                        <h3>{title}</h3>
+                        <div className="content">
+                            <div className="rating">
+                                <img src="star.svg" alt="Star Icon" />
+                                <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+                            </div>
+                            <span>•</span>
+                            <p className="lang">{original_language}</p>
+                            <span>•</span>
+                            <p className="year">{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        </li>
+    );
+};
 
-        <div className="content">
-            <div className="rating">
-                <img src="star.svg" alt="Star Icon" />
-                <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
-            </div>
-
-            <span>•</span>
-            <p className="lang">{original_language}</p>
-
-            <span>•</span>
-            <p className="year">
-                {release_date ? release_date.split('-')[0] : 'N/A'}
-            </p>
-        </div>
-    </div>
-</div>
-    )
-}
-export default MovieCard
+export default MovieCard;
